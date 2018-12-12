2ac7a0d49fc664f01a0c715790e1395427d91466
cyantarek <cyantarek.cg@gmail.com>
2018-12-12 16:30:38 +0600

    Test 2

--- main.go
+++ main.go
@@ -2,32 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 )
-
-// HelloWorld for hello world
-func HelloWorld() string {
-	return "Hello World, golang workshop"
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Println("http request....")
-	_, _ = fmt.Fprintf(w, "I love %s!", r.URL.Path[1:])
-}
-
-func pinger(port string) error {
-	resp, err := http.Get("http://localhost:" + port)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("server returned not-200 status code")
-	}
-
-	return nil
-}
 
 func main() {
 	fmt.Println("hello World Drone Go")
